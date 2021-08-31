1ed20e3415dffd1540ef53dd65f8a329c540eab3
Mariusz Slabicki <4083628+mariuszslabicki@users.noreply.github.com>
2021-08-31 10:52:41 +0200

    Add option to control number of workers in server and some small updates

--- server.py
+++ server.py
@@ -2,27 +2,29 @@ import simpy
 import time
 
 class Server(object):
-    def __init__(self, env):
+    def __init__(self, env, capacity = 1):
         self.start = time.perf_counter()
         self.env = env
-        self.q1 = simpy.Resource(env, capacity=1)
+        self.q1 = simpy.Resource(env, capacity=capacity)
         self.queueing_time = []
         self.processing_time = []
+        self.queue_length = {}
 
     def receive_report(self, nodeType, id, report, creationTime):
-        end = time.perf_counter()
-        print("RealTime", end - self.start)
-        print(self.env.now, "Received report from device", nodeType, id)
-        if len(report) > 0:
-            for entry in report:
-                print("Type", entry[0], "Dev:", entry[1], "\t measuredD:", entry[2], "\t lastRealD:", entry[3], "\t lastRealX:", entry[4], "\t lastRealY:", entry[5])
-        else:
-            print("Received empty report")
+        # end = time.perf_counter()
+        # print("RealTime", end - self.start)
+        # print(self.env.now, "Received report from device", nodeType, id)
+        # if len(report) > 0:
+            # for entry in report:
+                # print("Type", entry[0], "Dev:", entry[1], "\t measuredD:", entry[2], "\t lastRealD:", entry[3], "\t lastRealX:", entry[4], "\t lastRealY:", entry[5])
+        # else:
+            # print("Received empty report")
         self.env.process(self.process_report(report, creationTime))
 
     def process_report(self, report, creationTime):
         with self.q1.request() as process_report:
             yield process_report
             self.queueing_time.append(self.env.now - creationTime)
-            print("Time from report creation to processing was", self.env.now - creationTime)
+            # print("Time from report creation to processing was", self.env.now - creationTime)
+            self.queue_length[self.env.now] = len(self.q1.queue)
             yield self.env.timeout(10)
--- simulate.py
+++ simulate.py
@@ -1,33 +1,61 @@
 import server
 import network
 import simpy
+import matplotlib.pyplot as plt
 
-realTime = True
+realTime = False
 
-if realTime is False:
-    env = simpy.Environment()
-else:
-    env = simpy.RealtimeEnvironment(initial_time=0, factor=0.001, strict=False)
+number_of_devices = [100, 200, 300]
+capacity = [1, 2, 3, 4, 5]
+for cap in capacity:
+    for nod in number_of_devices:
+        print(nod, cap)
+        if realTime is False:
+            env = simpy.Environment()
+        else:
+            env = simpy.RealtimeEnvironment(initial_time=0, factor=0.001, strict=False)
 
-network = network.Network(env)
+        net = network.Network(env)
 
-srv = server.Server(env)
-network.add_server(srv)
+        srv = server.Server(env, capacity=cap)
+        net.add_server(srv)
 
-for i in range(5):
-    network.add_mobile_node(i)
+        for i in range(nod):
+            net.add_mobile_node(i)
 
-for i in range(1):
-    network.add_static_node(i)
+        for i in range(0):
+            net.add_static_node(i)
 
-# 1h        is 3600000  ms
-# 10min     is 600000   ms
-# 5min      is 300000   ms
+        # 1h        is 3600000  ms
+        # 10min     is 600000   ms
+        # 5min      is 300000   ms
 
-if realTime is False:
-    env.run(30000)
+        if realTime is False:
+            env.run(30000)
+        else:
+            env.run()
 
-else:
-    env.run()
+        plt.figure()
+        plt.hist(srv.queueing_time, density=False, bins=30)
+        plt.suptitle('Number or nodes ' + str(len(net.static_devices) + len(net.mobile_devices)))
+        plt.xlabel('Queueing time [ms]')
+        plt.ylabel('Number of events')
+        plt.title("Capacity " + str(cap))
+        plt.savefig(str(nod) + "_" + str(cap) + '_histogram.png')
+        plt.close()
 
-print(srv.queueing_time)
+        x = []
+        y = []
+
+        for key in srv.queue_length:
+            x.append(key)
+            y.append(srv.queue_length[key])
+
+        plt.figure()
+        plt.suptitle('Number or nodes ' + str(len(net.static_devices) + len(net.mobile_devices)))
+        plt.xlabel('Simulation time')
+        plt.ylabel('Queue length')
+        plt.title("Capacity " + str(cap))
+        plt.scatter(x, y)
+        plt.savefig(str(nod) + "_" + str(cap) + "_queue_length.png")
+        plt.close()
