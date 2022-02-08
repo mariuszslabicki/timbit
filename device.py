0f77b5ce7bc114fea5dad48b41fe2f44d03bf674
Konrad Połys <kpolys@iitis.pl>
2022-02-08 15:15:13 +0100

    not working

--- device.py
+++ device.py
@@ -5,7 +5,7 @@ import simpy
 
 
 class Device(object):
-    def __init__(self, env, id, config, mes_dimension, static=False):
+    def __init__(self, env, id, config, dict_dev, static=False):
         self.env = env
         self.id = id
         self.static = static
@@ -30,13 +30,13 @@ class Device(object):
         self.wrong_distance_classification_before_triangle = 0
         self.correct_distance_classification_after_triangle = 0
         self.wrong_distance_classification_after_triangle = 0
-        
+        self.dict_dev = dict_dev
         self.max_age_of_measurement = int(self.conf["max_age_of_measurement"])
-        self.packet_loss_probability = float(
-            self.conf["packet_loss_probability"])
+        self.packet_loss_probability = float(self.conf["packet_loss_probability"])
         # self.mes = [{[None for x in range(mes_dimension)]} for y in range(mes_dimension)]
         self.mes = {}
         self.env.process(self.transmit_ADV())
+        self.env.process(self.mesh_correction())
         self.env.process(self.perform_server_report())
         if self.static is False:
             self.env.process(self.keep_moving())
@@ -79,6 +79,9 @@ class Device(object):
                 if self.steps_to_WP != 0:
                     self.delta_x = (self.WP_x - self.x) / self.steps_to_WP
                     self.delta_y = (self.WP_y - self.y) / self.steps_to_WP
+                else:
+                    self.delta_x = 0.0
+                    self.delta_y = 0.0
 
             yield self.env.timeout(location_update_interval)
             self.make_a_step()
@@ -121,9 +124,9 @@ class Device(object):
                 self.mes[ids[0]] = {}
             if ids[1] in self.mes[ids[0]]:
                 old_val = self.mes[ids[0]][ids[1]][0]
-                self.mes[ids[0]][ids[1]] = [((0.5 * old_val) + calculated_dist) / 1.5, self.env.now]
+                self.mes[ids[0]][ids[1]] = [((0.5 * old_val) + calculated_dist) / 1.5, self.env.now, sender]
             else:
-                self.mes[ids[0]][ids[1]] = [calculated_dist, self.env.now]
+                self.mes[ids[0]][ids[1]] = [calculated_dist, self.env.now, sender]
             # mesh - direct neighbour - eng
 
             # mesh - neighbours of neighbour - begin
@@ -134,9 +137,9 @@ class Device(object):
                     self.mes[ids[0]] = {}
                 if ids[1] in self.mes[ids[0]]:
                     old_val = self.mes[ids[0]][ids[1]][0]
-                    self.mes[ids[0]][ids[1]] = [((0.5 * old_val) + sender.known_static_nodes[neighbour_id][2]) / 1.5, self.env.now]
+                    self.mes[ids[0]][ids[1]] = [((0.5 * old_val) + sender.known_static_nodes[neighbour_id][2]) / 1.5, self.env.now, sender]
                 else:
-                    self.mes[ids[0]][ids[1]] = [sender.known_static_nodes[neighbour_id][2], self.env.now]
+                    self.mes[ids[0]][ids[1]] = [sender.known_static_nodes[neighbour_id][2], self.env.now, sender]
                     
             for neighbour_id in sender.known_dynamic_nodes.keys():
                 ids = [sender_id_typed, str(neighbour_id)+'D']
@@ -145,13 +148,15 @@ class Device(object):
                     self.mes[ids[0]] = {}
                 if ids[1] in self.mes[ids[0]]:
                     old_val = self.mes[ids[0]][ids[1]][0]
-                    self.mes[ids[0]][ids[1]] = [((0.5 * old_val) + sender.known_dynamic_nodes[neighbour_id][2]) / 1.5, self.env.now]
+                    self.mes[ids[0]][ids[1]] = [((0.5 * old_val) + sender.known_dynamic_nodes[neighbour_id][2]) / 1.5, self.env.now, sender]
                 else:
-                    self.mes[ids[0]][ids[1]] = [sender.known_dynamic_nodes[neighbour_id][2], self.env.now]
+                    self.mes[ids[0]][ids[1]] = [sender.known_dynamic_nodes[neighbour_id][2], self.env.now, sender]
             # mesh - neighbours of neighbour - end
 
             self.make_distance_classification(sender, calculated_dist)
         else:
+            
+            #mesh - finding the lost neighbour - begin
             sender_id_typed = str(sender.id)+'U'
             if sender.static is False:
                 sender_id_typed = str(sender.id)+'D'
@@ -179,20 +184,7 @@ class Device(object):
                                 # print("\n", "Jestem:", self.id_typed, ", utracilem od:", sender_id_typed, ", obliczony dystans od RSSI: ", calculated_dist)
                                 # print("do sasiada:", mn, "jest:", self.mes[ids[0]][ids[1]][0], "(", ids[0], ids[1] ,")", "a od niego do", sender_id_typed, "jest", self.mes[idsn[0]][idsn[1]][0], "(", idsn[0], idsn[1] ,")",)
                                 self.make_distance_classification(sender, self.mes[ids[0]][ids[1]][0] + self.mes[idsn[0]][idsn[1]][0], True)
-        
-        sender_id_typed = str(sender.id)+'U'
-        if sender.static is False:
-            sender_id_typed = str(sender.id)+'D'
-        if sender.static is True:
-            sender_id_typed = str(sender.id)+'S'
-        
-        ids = [self.id_typed, sender_id_typed]
-        ids.sort()
-        if ids[0] in self.mes:
-            if ids[1] in self.mes[ids[0]]:
-                self.make_distance_classification(sender, self.mes[ids[0]][ids[1]][0], triangle=True, tri_before = True)
-                self.calculate_distance_in_triangle()
-                self.make_distance_classification(sender, self.mes[ids[0]][ids[1]][0], triangle=True, tri_before = False)
+            #mesh - finding the lost neighbour - end
 
 
     def perform_server_report(self):
@@ -233,7 +225,7 @@ class Device(object):
             delta = 0
             yield self.env.timeout(1000 + delta)
 
-    def make_distance_classification(self, sender, calculated_dist, neighbour=False, triangle=False, tri_before = False):
+    def make_distance_classification(self, sender, calculated_dist, neighbour=False):
         real_dist = round(math.sqrt((sender.x-self.x) **
                           2 + (sender.y-self.y)**2), 2)
         sender_id_typed = str(sender.id)+'U'
@@ -242,7 +234,7 @@ class Device(object):
         else:
             sender_id_typed = str(sender.id)+'S'
 
-        if neighbour is False and triangle is False:
+        if neighbour is False:
             if (real_dist < 5 and calculated_dist < 5.0) or (real_dist >= 5.0 and calculated_dist >= 5.0):
                 self.correct_distance_classification += 1
             else:
@@ -262,22 +254,44 @@ class Device(object):
             else:
                 # print(real_dist, calculated_dist)
                 self.wrong_distance_classification_neighbour += 1
-        if triangle is True:
-            if tri_before is True:
-                if (real_dist < 5 and calculated_dist < 5.0) or (real_dist >= 5 and calculated_dist >= 5.0):
-                    self.correct_distance_classification_before_triangle += 1
-                else:
-                    # print(real_dist, calculated_dist)
-                    self.wrong_distance_classification_before_triangle += 1
-            if tri_before is False:
-                if (real_dist < 5 and calculated_dist < 5.0) or (real_dist >= 5 and calculated_dist >= 5.0):
-                    self.correct_distance_classification_after_triangle += 1
-                else:
-                    # print(real_dist, calculated_dist)
-                    self.wrong_distance_classification_after_triangle += 1
-                
     
-    def calculate_distance_in_triangle(self):
+    def mesh_correction(self):
+        while True: 
+            # self.triangle_correction()
+            self.triangle_correction_eval()
+            print(self.id_typed, self.env.now)
+            yield self.env.timeout(100)
+    
+    def triangle_correction_eval(self):
+        all_ids = []
+        for key1, val1 in self.mes.items():
+            if key1 not in all_ids:
+                all_ids.append(key1)
+            for key2 in val1:
+                if key2 not in all_ids:
+                    all_ids.append(key2)
+        all_ids.sort()
+        id1 = self.id_typed
+        for id2 in all_ids:
+            if id2 != id1:
+                ids1_2 = [id1, id2]
+                ids1_2.sort()
+                if ids1_2[0] in self.mes:
+                    if ids1_2[1] in self.mes[ids1_2[0]]:
+                        dist1_2 = self.mes[ids1_2[0]][ids1_2[1]][0]
+                        real_dist = math.hypot(self.x - self.dict_dev[id2].x, self.y - self.dict_dev[id2].y)
+                        # print(ids1_2, dist1_2, id2, real_dist)
+                        if (real_dist < 5 and dist1_2 < 5.0) or (real_dist >= 5 and dist1_2 >= 5.0):
+                            self.correct_distance_classification_before_triangle += 1
+                        else:
+                            self.wrong_distance_classification_before_triangle += 1
+                        
+                        
+
+        
+        
+    
+    def triangle_correction(self):
         all_ids = []
         for key1, val1 in self.mes.items():
             if key1 not in all_ids:
@@ -288,40 +302,40 @@ class Device(object):
         all_ids.sort()
         
         triangle_list = []
-        for id1 in all_ids:
-            for id2 in all_ids:
-                if id2 != id1:
-                    ids1_2 = [id1, id2]
-                    ids1_2.sort()
-                    if ids1_2[0] in self.mes:
-                        if ids1_2[1] in self.mes[ids1_2[0]]:
-                            #pierwszy bok ids1_2
-                            for id3 in all_ids:
-                                if id3 != id2 and id3 != id1:
-                                    ids2_3 = [id2, id3]
-                                    ids2_3.sort()
-                                    if ids2_3[0] in self.mes:
-                                        if ids2_3[1] in self.mes[ids2_3[0]]:
-                                            #drugi bok ids2_3
-                                            ids3_1 = [id3, id1]
-                                            ids3_1.sort()
-                                            if ids3_1[0] in self.mes:
-                                                if ids3_1[1] in self.mes[ids3_1[0]]:
-                                                    #trzeci bok ids3_1
-                                                    # print(ids1_2, ids2_3, ids3_1)
-                                                    dist1_2 = self.mes[ids1_2[0]][ids1_2[1]][0]
-                                                    dist2_3 = self.mes[ids2_3[0]][ids2_3[1]][0]
-                                                    dist3_1 = self.mes[ids3_1[0]][ids3_1[1]][0]
-                                                    dists = [dist1_2, dist2_3, dist3_1]
-                                                    dists.sort()
-                                                    if dists[0] + dists[1] < dists[2]: #zle trojkaty
-                                                        ids = [ids1_2, ids2_3, ids3_1]
-                                                        ids.sort()
-                                                        if ids not in triangle_list:
-                                                            triangle_list.append( ids )
-                                                            # print(ids, round(dist1_2,2), round(dist2_3,2), round(dist3_1,2))
+        id1 = self.id_typed
+        for id2 in all_ids:
+            if id2 != id1:
+                ids1_2 = [id1, id2]
+                ids1_2.sort()
+                if ids1_2[0] in self.mes:
+                    if ids1_2[1] in self.mes[ids1_2[0]]:
+                        #pierwszy bok ids1_2
+                        for id3 in all_ids:
+                            if id3 != id2 and id3 != id1:
+                                ids2_3 = [id2, id3]
+                                ids2_3.sort()
+                                if ids2_3[0] in self.mes:
+                                    if ids2_3[1] in self.mes[ids2_3[0]]:
+                                        #drugi bok ids2_3
+                                        ids3_1 = [id3, id1]
+                                        ids3_1.sort()
+                                        if ids3_1[0] in self.mes:
+                                            if ids3_1[1] in self.mes[ids3_1[0]]:
+                                                #trzeci bok ids3_1
+                                                # print(ids1_2, ids2_3, ids3_1)
+                                                dist1_2 = self.mes[ids1_2[0]][ids1_2[1]][0]
+                                                dist2_3 = self.mes[ids2_3[0]][ids2_3[1]][0]
+                                                dist3_1 = self.mes[ids3_1[0]][ids3_1[1]][0]
+                                                dists = [dist1_2, dist2_3, dist3_1]
+                                                dists.sort()
+                                                if dists[0] + dists[1] < dists[2]: #zle trojkaty
+                                                    ids = [ids1_2, ids2_3, ids3_1]
+                                                    ids.sort()
+                                                    if ids not in triangle_list:
+                                                        triangle_list.append( ids )
+                                                        # print(ids, round(dist1_2,2), round(dist2_3,2), round(dist3_1,2))
 
-                                                        #[dist1_2, dist2_3, dist3_1]
+                                                    #[dist1_2, dist2_3, dist3_1]
         triangle_list.sort()
         if len(triangle_list) > 0:
             out = {}
@@ -329,17 +343,15 @@ class Device(object):
                 if str(line[0]) not in out:
                     out[str(line[0])] = []
                 out[str(line[0])].append(line)
-            # print("++++++++++++++++++++")
             
             for key, val in out.items():
-                # print(key, ":")
-                # print(" ")
+                print(key, ":")
                 biggest = True
                 smallest = True
                 target_bval = 9999.0
                 target_mval = 0.0
                 for v in val:
-                    # print("\t", v)
+                    print("\t", v)
                     dist1_2 = self.mes[v[0][0]][v[0][1]][0]
                     dist2_3 = self.mes[v[1][0]][v[1][1]][0]
                     dist3_1 = self.mes[v[2][0]][v[2][1]][0]
@@ -353,13 +365,14 @@ class Device(object):
                     else:
                         if target_bval > dist2_3 + dist3_1:
                             target_bval = dist2_3 + dist3_1
-                    # print(key, round(dist1_2,2), round(dist2_3,2), round(dist3_1,2))
+                    # print("\t", round(dist1_2,2),"\t\t", round(dist2_3,2),"\t\t", round(dist3_1,2))
                 if biggest:
                     self.mes[val[0][0][0]][val[0][0][1]][0] = target_bval-0.01
-                    # print(key, "biggest", val[0][0][0], val[0][0][1], self.mes[val[0][0][0]][val[0][0][1]][0] )
+                    # print("\t", "biggest", val[0][0][0], val[0][0][1], self.mes[val[0][0][0]][val[0][0][1]][0] )
                 if smallest:
                     self.mes[val[0][0][0]][val[0][0][1]][0] = target_mval+0.01
-                    # print(key, "smallest", val[0][0][0], val[0][0][1], self.mes[val[0][0][0]][val[0][0][1]][0] )    
+                    # print("\t", "smallest", val[0][0][0], val[0][0][1], self.mes[val[0][0][0]][val[0][0][1]][0] ) 
+                # print("\n")   
                     
             
             
--- network.py
+++ network.py
@@ -6,29 +6,30 @@ class Network(object):
     def __init__(self, env, config, pathloss_model):
         self.mobile_devices = []
         self.static_devices = []
+        self.dictionary_devices = {}
         self.env = env
         self.config = config
         self.x_size = int(self.config["network_size_x"])
         self.y_size = int(self.config["network_size_y"])
-        self.mes_dimension = int(self.config["number_of_mobile_dev"]) + int(self.config["number_of_static_dev"])
+        # self.mes_dimension = int(self.config["number_of_mobile_dev"]) + int(self.config["number_of_static_dev"])
         self.pathloss_model = pathloss_model
         if self.pathloss_model == "matrix_based":
             import pathloss_matrix
             self.obstacle_calc = pathloss_matrix.PathlossCalculator(self.x_size+2, self.y_size+2)
 
     def add_mobile_node(self, id):
-        dev = device.Device(self.env, id, self.config, self.mes_dimension)
+        dev = device.Device(self.env, id, self.config, self.dictionary_devices)
         self.mobile_devices.append(dev)
-        dev.network = self
+        self.dictionary_devices[str(id)+'D'] = dev
         dev.x_limit = self.x_size
         dev.y_limit = self.y_size
         dev.x = random.randint(0, dev.x_limit)
         dev.y = random.randint(0, dev.y_limit)
 
     def add_static_node(self, id):
-        dev = device.Device(self.env, id, self.config, self.mes_dimension, static=True)
+        dev = device.Device(self.env, id, self.config, self.dictionary_devices)
         self.static_devices.append(dev)
-        dev.network = self
+        self.dictionary_devices[str(id)+'S'] = dev
         dev.x_limit = self.x_size
         dev.y_limit = self.y_size
         dev.x = random.randint(0, dev.x_limit)
